5efc878bcc830ad9a83be20fa9b774a7ec1fb23d
linhao <linhao.4zhong@163.com>
2019-08-14 17:10:52 +0800

    添加脚本采集与脚本执行功能

--- test.js
+++ test.js
@@ -13,15 +13,43 @@
 //     console.log( 'message', data, flags );
 // } );
 
-const ioc = require( 'socket.io-client' );
+// const ioc = require( 'socket.io-client' );
 
-const socket = ioc( 'http://node.zuzuche.net:3000/node-monitor' );
-    // 链接成功
-    socket.on( 'connect', () => {
-        console.log('链接成功');
-        socket.send('message', '123123123');
-    } );
+// const socket = ioc( 'http://node.zuzuche.net:3000/node-monitor' );
+//     // 链接成功
+//     socket.on( 'connect', () => {
+//         console.log('链接成功');
+//         socket.send('message', '123123123');
+//     } );
 
-    socket.on('error', (error) => {
-        console.log('链接出错：',error);
-    });
+//     socket.on('error', (error) => {
+//         console.log('链接出错：',error);
+//     });
+
+// var execFileSync = require('child_process').execFileSync;
+const fs = require('fs');
+// var result = execFileSync('/Users/lamho/Desktop/前端代码/eagleeye/.task/1565691065244.sh');
+// console.log( new Buffer(result).toString() );
+const file = '/Users/lamho/Desktop/前端代码/eagleeye/shell/test.sh';
+fs.open( file, 'r', function ( err, fd ) {
+    if ( err ) {
+      console.log(1);
+      return;
+    }
+
+    // fs.fchmod( fd, '0777', function ( err ) {
+    //   if ( err ) {
+    //     console.log(2);
+    //     return;
+    //   }
+      
+    //   fs.close(fd, function(err) {
+    //     if ( err ) {
+    //         console.log(3);
+    //       return;
+    //     }
+
+    //     console.log(4)
+    //   });
+    // } );
+  } );
